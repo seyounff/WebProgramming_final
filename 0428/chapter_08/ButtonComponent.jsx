6a5d828a23704ebd861b92d0e7b5886dc3c7fe95
seyounff <161145583+seyounff@users.noreply.github.com>
2025-05-06 22:17:31 +0900

    Add files via upload

---
+++ 0428/chapter_08/ButtonComponent.jsx
@@ -0,0 +1,9 @@
+function ButtonComponent() {
+  function handleClick() {
+    alert("버튼이 클릭되었습니다");
+  }
+
+  return <button onClick={handleClick}>클릭</button>;
+}
+
+export default ButtonComponent;
---
+++ 0428/chapter_08/InputComponent.jsx
@@ -0,0 +1,9 @@
+function InputComponent() {
+  function handleChange(event) {
+    console.log("입력값:", event.target);
+  }
+
+  return <input type="text" onChange={handleChange} />;
+}
+
+export default InputComponent;
